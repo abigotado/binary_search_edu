ec84ebb6d696783fd30ea4cbd37ec275481c4639
Nikita Kovalenko <nik.koval.89@gmail.com>
2024-11-21 15:11:43 -0300

    Create collections class and methods

--- src/main/java/org/abigotado/collections/Collections.java
+++ src/main/java/org/abigotado/collections/Collections.java
@@ -1,4 +1,32 @@
 package org.abigotado.collections;
 
+import java.util.Comparator;
+import java.util.List;
+
 public class Collections {
+
+    // Binary search for list without comparator
+    public static <T extends Comparable<? super T>> int binarySearch(List<T> list, T key) {
+        return binarySearch(list, key, Comparator.naturalOrder());
+    }
+
+    // Binary search for list with comparator
+    public static <T> int binarySearch(List<T> list, T key, Comparator<? super T> c) {
+        int low = 0;
+        int high = list.size() - 1;
+
+        while (low <= high) {
+            int mid = (low + high) >>> 1;
+            T midVal = list.get(mid);
+            int cmp = c.compare(midVal, key);
+
+            if (cmp < 0)
+                low = mid + 1;
+            else if (cmp > 0)
+                high = mid - 1;
+            else
+                return mid;
+        }
+        return -(low + 1);
+    }
 }
